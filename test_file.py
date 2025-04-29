391dadcee4d09f4f75cbc0259c8f6213aa352b8a
Arushi9740 <arushimuskan004@gmail.com>
2025-04-29 14:35:44 +0530

    adding file

--- test_file.py
+++ test_file.py
@@ -2,7 +2,9 @@ from user import create_user
 
 def test_case():
     assert create_user('"abc"',"abc@gmail.com","9036046474",5263965) == "User account created successfully"
-    # assert create_user("def","def@gmail.com","88840103",123) == "Password is too short"
+
+def test_case_invalid():
+    assert create_user("def","def@gmail.com","88840103",123) == "Password is too short"
 
 print("Test passed")
 
